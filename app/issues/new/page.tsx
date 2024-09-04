f8f2265fee7fc4d0171047fc6092aa106e996f1a
Sourav Goel <64015479+sourav-9091@users.noreply.github.com>
2024-09-04 17:15:01 +0530

    initial commit

---
+++ app/issues/new/page.tsx
@@ -0,0 +1,11 @@
+import { TextField } from '@radix-ui/themes'
+import React from 'react'
+
+export const NewIssuePage = () => {
+  return (
+    <div>
+        <TextField.Root>
+        </TextField.Root>
+    </div>
+  )
+}
---
+++ app/issues/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Button } from '@radix-ui/themes';
+
+export default function Issues() {
+    return (
+      <div><Button>New Issue</Button></div>
+    );
+  }
+  
